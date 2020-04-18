566be0b3d21648558ef5a09073998624f51104d1
Pavel Karkh <pavelkarkh@jetstyle.ru>
2020-04-18 18:01:08 +0500

    и так сойдет

--- src/tree.go
+++ src/tree.go
@@ -18,9 +18,9 @@ func main() {
 
 }
 
-func recursiveDir(dir string, path string, counter int32) {
+func recursiveDir(dir string, file string, counter int32) {
 	intCounter := counter
-	fullPath := filepath.Join(dir, path)
+	fullPath := filepath.Join(dir, file)
 	if _, err := os.Stat(fullPath); os.IsNotExist(err) {
 		fmt.Println("path is not exist")
 	}
@@ -30,12 +30,18 @@ func recursiveDir(dir string, path string, counter int32) {
 	}
 	for _, file := range files {
 		if file.IsDir() {
+			printRow(intCounter, file.Name())
 			recursiveDir(fullPath, file.Name(), intCounter+1)
+		} else {
+			printRow(intCounter, file.Name())
 		}
-		var i int32
-		for i = 0; i < intCounter; i++ {
-			fmt.Print("-")
-		}
-		fmt.Println(file.Name())
 	}
+}
+
+func printRow(counter int32, name string) {
+	var i int32
+	for i = 0; i < counter; i++ {
+		fmt.Print("-")
+	}
+	fmt.Println(name)
 }
